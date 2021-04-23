fe4c2b404b1984d68435babb9cbe61e59f7c6beb
siman <simanshrivastav20@gmail.com>
2021-04-22 21:55:21 -0400

    Task one done

--- src/Assignments/Divisible.java
+++ src/Assignments/Divisible.java
@@ -1,5 +1,7 @@
 package Assignments;
 
+import java.util.Scanner;
+
 public class Divisible {
     public static void main(String[] args) {
 //        int i;
@@ -12,27 +14,51 @@ public class Divisible {
 //            }
 //            else if (i%5==0){
 //                System.out.println("JAVA Training");
-
-
-        int integer1;
-        integer1 = 0;
-        boolean divisibleby3 = (integer1 % 3) == 0;
-        boolean divisibleby5 = (integer1 % 5) == 0;
-
-        for( integer1=0; integer1<1000; integer1++){
-            if(divisibleby3 = true);{
-                System.out.println(integer1 + " can be divided by 3");}
-            if(divisibleby3 = false);{
-                System.out.println(integer1 + " cannot be divided by 3");}
-            if(divisibleby5 = true);{
-                System.out.println(integer1 + " can be divided by 5");}
-            if(divisibleby5 = false);{
-                System.out.println(integer1 + " cannot be divided by 5");
-
-
+        System.out.println("Enter a number:");
+            Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int userValue = scanner.nextInt();
+            if(userValue==0){
+                System.exit(0);
             }
+            if (userValue % 5 == 0 && userValue % 3 == 0) {
 
+                System.out.println("Consultadd JAVA Training");
+            } else if (userValue % 3 == 0) {
+                System.out.println("Consultadd");
+            } else if (userValue % 5 == 0) {
+                System.out.println("JAVA Training");
+            }else{
+                System.out.println("This no is not divisible by 5 and 3");
             }
         }
     }
+
+
+
+
+
+
+
+
+//
+//
+//        int integer1;
+//        integer1 = 0;
+//        boolean divisibleby3 = (integer1 % 3) == 0;
+//        boolean divisibleby5 = (integer1 % 5) == 0;
+//
+//        for( integer1=0; integer1<1000; integer1++){
+//            if(divisibleby3 = true);{
+//                System.out.println(integer1 + " can be divided by 3");}
+//            if(divisibleby3 = false);{
+//                System.out.println(integer1 + " cannot be divided by 3");}
+//            if(divisibleby5 = true);{
+//                System.out.println(integer1 + " can be divided by 5");}
+//            if(divisibleby5 = false);{
+//                System.out.println(integer1 + " cannot be divided by 5");
+
+
+            }
+
 
--- src/Assignments/OneToTen.java
+++ src/Assignments/OneToTen.java
@@ -3,16 +3,24 @@ package Assignments;
 import java.util.Scanner;
 
 public class OneToTen {
-    public static void main(String[] args) {
-        int b[]={1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] arg10s) {
+//        int b[]={1,2,3,4,5,6,7,8,9,10};
 
-        int z;
-        System.out.println("Enter a number : "+ b);
-        System.out.println("Enter a number : "+ b);
+
+        System.out.println("Enter a number between 1-10 : ");
         Scanner in = new Scanner(System.in);
-        z = in.nextInt();
-        System.out.println("Entered integer is: "
-                + z);
+       int a = in.nextInt();
+        System.out.println("Enter second number 1-10 : ");
+       int  b = in. nextInt();
+        if(a<=10 || b<=10){
+            int z = a+b;
+            System.out.println("Your sum without adding 30: "+z);
+            int k = z+30;
+            System.out.println("Your sum after adding 30: "+k);
+        }else{
+            System.out.println("You entered more value");
+
+        }
 
     }
 }
